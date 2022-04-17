eb10007fcc1654b7cde35dac94be471b8e76bdb5
1sixth <1sixth@shinta.ro>
2022-04-18 02:48:54 +0800

    tyo1, tyo2: add qbittorrent-nox module and overlay

--- nixos/tyo2/default.nix
+++ nixos/tyo2/default.nix
@@ -3,9 +3,11 @@
 nixpkgs.lib.nixosSystem {
   inherit system;
   modules = [
+    { nixpkgs.overlays = [ self.overlays.qbittorrent-nox ]; }
     inputs.nixos-cn.nixosModules.nixos-cn
     inputs.sops-nix.nixosModules.sops
     self.nixosModules.dnscrypt-proxy2.earth
+    self.nixosModules.qbittorrent-nox
     self.nixosModules.server
     self.nixosModules.tor
     self.nixosModules.v2ray.server
