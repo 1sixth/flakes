82916b661849e051e97bb3cfc7346e615f02cad5
1sixth <1sixth@shinta.ro>
2022-04-23 18:12:48 +0800

    toy: add substituters and trusted-public-keys

--- nixos/toy/configuration.nix
+++ nixos/toy/configuration.nix
@@ -75,6 +75,8 @@
     auto-optimise-store = true;
     builders-use-substitutes = true;
     experimental-features = [ "flakes" "nix-command" ];
+    substituters = [ "https://hydra.shinta.ro" ];
+    trusted-public-keys = [ "hydra.shinta.ro:nxVZvKRpeiGdqeiHJ7QRm4GYOF/7BuFpOnaPULw+QX4=" ];
     trusted-users = [ "one6th" "root" ];
   };
 
