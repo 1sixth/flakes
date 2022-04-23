8bce6056d3688254c28bdaf76a3e9acff1412f5c
1sixth <1sixth@shinta.ro>
2022-04-24 02:11:01 +0800

    toy: vscodium: add gitlens

--- nixos/toy/home/vscodium.nix
+++ nixos/toy/home/vscodium.nix
@@ -6,6 +6,7 @@
     extensions = with pkgs.vscode-extensions; [
       davidanson.vscode-markdownlint
       dotjoshjohnson.xml
+      eamodio.gitlens
       jnoortheen.nix-ide
       llvm-vs-code-extensions.vscode-clangd
       matklad.rust-analyzer
