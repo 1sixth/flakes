931a7f996124dac6247fa1ff0c78df17c599b16f
1sixth <1sixth@shinta.ro>
2022-04-26 11:52:38 +0800

    modules: add hath

--- modules/default.nix
+++ modules/default.nix
@@ -5,6 +5,7 @@
     china = import ./dnscrypt-proxy2/china.nix;
     earth = import ./dnscrypt-proxy2/earth.nix;
   };
+  hath = import ./hath.nix;
   qbittorrent-nox = import ./qbittorrent-nox.nix;
   server = import ./server;
   tor = import ./tor.nix;
---
+++ modules/hath.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, ... }:
+
+{
+  sops.secrets.hath = {
+    owner = config.users.users.hath.name;
+    inherit (config.users.users.hath) group;
+    restartUnits = [ "hath.service" ];
+  };
+
+  # https://aur.archlinux.org/cgit/aur.git/tree/hath.service?h=hath&id=dff03029064eb52d8c36073806b7eab5e3d75003
+  systemd.services.hath = {
+    description = "Hentai@Home service";
+    after = [ "network.target" ];
+    wants = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+
+    preStart = ''
+      mkdir -p /var/lib/hath/data
+      ln -sf ${config.sops.secrets.hath.path} /var/lib/hath/data/client_login
+    '';
+
+    serviceConfig = {
+      User = "hath";
+      StateDirectory = "hath";
+      WorkingDirectory = "/var/lib/hath/";
+      ExecStart = "${pkgs.HentaiAtHome}/bin/HentaiAtHome";
+      SuccessExitStatus = 143;
+      TimeoutStopSec = 10;
+      Restart = "always";
+      RestartSec = 5;
+    };
+  };
+
+  users = {
+    groups.hath = { };
+    users.hath = {
+      group = "hath";
+      isSystemUser = true;
+    };
+  };
+}
