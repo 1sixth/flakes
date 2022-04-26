82bf85dedaf8e7e29e59683170dfb0a739a3a277
1sixth <1sixth@shinta.ro>
2022-04-26 11:29:22 +0800

    modules: dnscrypt-proxy2.earth: use server_names

--- modules/dnscrypt-proxy2/earth.nix
+++ modules/dnscrypt-proxy2/earth.nix
@@ -6,12 +6,14 @@
   services = {
     dnscrypt-proxy2 = {
       enable = true;
-      settings = {
-        bootstrap_resolvers = [ "[2606:4700:4700::1111]:53" "[2606:4700:4700::1001]:53" "1.1.1.1:53" "1.0.0.1:53" ];
-        dnscrypt_servers = false;
-        ipv6_servers = true;
-        require_dnssec = true;
-      };
+      settings.server_names = [
+        "cloudflare"
+        "cloudflare-ipv6"
+        "google"
+        "google-ipv6"
+        "quad9-doh-ip4-port443-filter-pri"
+        "quad9-doh-ip6-port443-filter-pri"
+      ];
     };
     resolved.enable = false;
   };
