dfb496e2d61f427dd4dc3186586a1f69b17e7ae7
1sixth <1sixth@shinta.ro>
2022-04-27 23:51:49 +0800

    tyo0: reinstall

--- nixos/tyo0/hardware.nix
+++ nixos/tyo0/hardware.nix
@@ -4,23 +4,27 @@
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
 
   boot = {
-    initrd.kernelModules = [ "nvme" ];
-    loader.grub = {
-      efiSupport = true;
-      efiInstallAsRemovable = true;
-      device = "nodev";
+    initrd.availableKernelModules = [ "xhci_pci" "usbhid" ];
+    loader = {
+      grub.enable = false;
+      systemd-boot = {
+        editor = false;
+        enable = true;
+      };
     };
   };
 
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-uuid/38fc8aab-bb15-47ec-97a4-a8209a676d7b";
-      fsType = "xfs";
+      device = "/dev/disk/by-uuid/d2e52659-4f5c-483e-a7c6-f896e2fca3cc";
+      fsType = "btrfs";
+      options = [ "compress-force=zstd" "noatime" "space_cache=v2" ];
     };
     "/boot" = {
-      device = "/dev/disk/by-uuid/BB6A-4956";
+      device = "/dev/disk/by-uuid/2B35-2395";
       fsType = "vfat";
     };
   };
-  swapDevices = [{ device = "/dev/disk/by-uuid/041e6e37-8fdb-48c0-92c9-b238ffa90d4c"; }];
+
+  swapDevices = [{ device = "/dev/disk/by-uuid/a2ecf42c-fa70-49aa-ace7-b2381acaa80b"; }];
 }
