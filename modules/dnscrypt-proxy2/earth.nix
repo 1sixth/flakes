f522cabd482fcfeae2bf99a20ed93d8302cf8b5e
1sixth <1sixth@shinta.ro>
2022-05-02 11:58:18 +0800

    module: dnscrypt-proxy2.earth: remove quad9

--- modules/dnscrypt-proxy2/earth.nix
+++ modules/dnscrypt-proxy2/earth.nix
@@ -11,8 +11,6 @@
         "cloudflare-ipv6"
         "google"
         "google-ipv6"
-        "quad9-doh-ip4-port443-filter-pri"
-        "quad9-doh-ip6-port443-filter-pri"
       ];
     };
     resolved.enable = false;
