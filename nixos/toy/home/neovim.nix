b7c42f0ff1c4ed8e1793be5e611350c1f918f313
1sixth <1sixth@shinta.ro>
2022-05-11 14:19:45 +0800

    toy: rewrite neovim config
    It's kind of frustrating to set up lsp for neovim. I guess I'll
    just use neovim as a lightweight TUI editor.

--- nixos/toy/home/neovim.nix
+++ nixos/toy/home/neovim.nix
@@ -4,62 +4,31 @@
   programs.neovim = {
     enable = true;
     extraConfig = ''
-      " use true color
-      set termguicolors
-
-      " show line number
-      set number
-
-      " show relative line number
-      set relativenumber
-
-      " disable showmode because the mode information is displayed in the statusline
-      set noshowmode
-
-      " ignore case when searching
-      set ignorecase
-
-      " when searching try to be smart about cases
-      set smartcase
-
-      " show matching brackets when text indicator is over them
-      set showmatch
-
-      " use spaces instead of tabs
-      set expandtab
-
-      " 1 tab == 4 spaces
-      set tabstop=4
-
-      " enable Smart indent
-      set smartindent
-
-      " move a line of text using ALT+[jk]
-      nmap <M-j> mz:m+<cr>`z
-      nmap <M-k> mz:m-2<cr>`z
-
-      " only use dark background in tty
-      if $TERM != 'linux'
-        set background=light
-      endif
+      lua << EOF
+      ${builtins.readFile ./res/neovim.lua}
+      EOF
     '';
     plugins = with pkgs.vimPlugins; [
-      {
-        config = ''
-          let g:lightline = {
-                \ 'colorscheme': 'PaperColor',
-                \ }
-        '';
-        plugin = lightline-vim;
-      }
-      {
-        config = "colorscheme PaperColor";
-        plugin = papercolor-theme;
-      }
-      vim-gitgutter
-      vim-polyglot
+      lualine-nvim
+      papercolor-theme
+      vim-lastplace
+      which-key-nvim
+      (nvim-treesitter.withPlugins (
+          plugins: with plugins; [
+            tree-sitter-c
+            tree-sitter-html
+            tree-sitter-json
+            tree-sitter-markdown
+            tree-sitter-nix
+            tree-sitter-lua
+            tree-sitter-python
+            tree-sitter-rust
+            tree-sitter-yaml
+          ]
+      ))
     ];
     viAlias = true;
     vimAlias = true;
+    withRuby = false;
   };
 }
---
+++ nixos/toy/home/res/neovim.lua
@@ -0,0 +1,33 @@
+vim.opt.background = 'light'
+vim.opt.expandtab = true
+vim.opt.ignorecase = true
+vim.opt.number = true
+vim.opt.shiftwidth = 4
+vim.opt.showmode = false
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.tabstop = 4
+vim.opt.termguicolors = true
+
+vim.cmd([[ colorscheme PaperColor ]])
+
+require('nvim-treesitter.configs').setup {
+  highlight = {
+    enable = true,
+  },
+  incremental_selection = {
+    enable = true,
+  },
+  indent = {
+    enable = true,
+  }
+}
+
+require('lualine').setup {
+  options = {
+    icons_enabled = false,
+    theme = 'PaperColor',
+  }
+}
+require("which-key").setup {
+}
