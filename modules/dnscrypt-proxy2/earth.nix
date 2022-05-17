66166b552696cc32226922f5d1972de2b75d152d
1sixth <1sixth@shinta.ro>
2022-05-17 17:05:07 +0800

    modules: dnscrypt-proxy2.earth: enable ipv6
    I can't believe why it isn't enabled by default.

--- modules/dnscrypt-proxy2/earth.nix
+++ modules/dnscrypt-proxy2/earth.nix
@@ -6,12 +6,15 @@
   services = {
     dnscrypt-proxy2 = {
       enable = true;
-      settings.server_names = [
-        "cloudflare"
-        "cloudflare-ipv6"
-        "google"
-        "google-ipv6"
-      ];
+      settings = {
+        ipv6_servers = true;
+        server_names = [
+          "cloudflare"
+          "cloudflare-ipv6"
+          "google"
+          "google-ipv6"
+        ];
+      };
     };
     resolved.enable = false;
   };
