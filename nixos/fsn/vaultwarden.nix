0f9dd4a02851aa0fb47b95ae822bad6dd63c6656
1sixth <1sixth@shinta.ro>
2022-05-19 16:21:11 +0800

    fsn: vaultwarden: disable websocket

--- nixos/fsn/vaultwarden.nix
+++ nixos/fsn/vaultwarden.nix
@@ -5,20 +5,7 @@
     # https://github.com/dani-garcia/vaultwarden/wiki/Proxy-examples
     nginx.virtualHosts."vault.shinta.ro" = {
       forceSSL = true;
-      locations = {
-        "/" = {
-          proxyPass = "http://127.0.0.1:8000";
-          proxyWebsockets = true;
-        };
-        "/notifications/hub" = {
-          proxyPass = "http://127.0.0.1:3012";
-          proxyWebsockets = true;
-        };
-        "/notifications/hub/negotiate" = {
-          proxyPass = "http://127.0.0.1:8000";
-          proxyWebsockets = true;
-        };
-      };
+      locations."/".proxyPass = "http://127.0.0.1:8000";
       useACMEHost = "shinta.ro";
     };
     vaultwarden = {
