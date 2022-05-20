e3f9e95089b6b7b19531b235b14f2110a880a5ae
1sixth <1sixth@shinta.ro>
2022-05-20 13:54:59 +0800

    fsn: move programs.ssh and nix back to configuration.nix

--- nixos/fsn/configuration.nix
+++ nixos/fsn/configuration.nix
@@ -23,9 +23,68 @@
 
   networking.hostName = "fsn";
 
+  nix = {
+    buildMachines = [
+      {
+        hostName = "fsn";
+        maxJobs = 8;
+        supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+        systems = [
+          "i686-linux"
+          "x86_64-linux"
+        ];
+      }
+      {
+        hostName = "tyo0";
+        supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" "gccarch-armv8-a" ];
+        systems = [
+          "aarch64-linux"
+        ];
+      }
+      {
+        hostName = "tyo3";
+        supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" "gccarch-armv8-a" ];
+        systems = [
+          "aarch64-linux"
+        ];
+      }
+    ];
+    distributedBuilds = true;
+    settings.builders-use-substitutes = true;
+  };
+
+  programs.ssh = {
+    extraConfig = ''
+      Host fsn
+        HostName fsn.9875321.xyz
+        IdentityFile ${config.sops.secrets.ssh_private_key.path}
+        User root
+      Host tyo0
+        HostName tyo0.9875321.xyz
+        IdentityFile ${config.sops.secrets.ssh_private_key.path}
+        User root
+      Host tyo3
+        HostName tyo3.9875321.xyz
+        IdentityFile ${config.sops.secrets.ssh_private_key.path}
+        User root
+    '';
+    knownHosts = {
+      "fsn.9875321.xyz".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAss7ohT8nmKrKCtz2ca4MT98mDj/O0Ti+WxqCKI/+ba";
+      "tyo0.9875321.xyz".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIILj2hY2QVnysE20yMSWzMyORXPs+LjbMi2GIzQXQuJO";
+      "tyo3.9875321.xyz".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBQzz4TIaV597J0WfLYnmq9z4HcbddX/bBRXQctZVbhK";
+    };
+  };
+
   security.acme.certs."shinta.ro".extraDomainNames = [ "*.shinta.ro" ];
 
-  sops.defaultSopsFile = ./secrets.yaml;
+  sops = {
+    defaultSopsFile = ./secrets.yaml;
+    secrets.ssh_private_key = {
+      mode = "0440";
+      owner = config.users.users.hydra.name;
+      inherit (config.users.users.hydra) group;
+    };
+  };
 
   systemd.network.networks.default = {
     address = [ "2a01:4f8:172:3c52::1/64" ];
