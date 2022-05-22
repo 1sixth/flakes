d078bd3a4df72c32c3c482d9f81311e3177ccf46
1sixth <1sixth@shinta.ro>
2022-05-22 17:03:41 +0800

    modules: dnscrypt-proxy2.earth: configure bootstrap resolvers

--- modules/dnscrypt-proxy2/earth.nix
+++ modules/dnscrypt-proxy2/earth.nix
@@ -7,6 +7,12 @@
     dnscrypt-proxy2 = {
       enable = true;
       settings = {
+        bootstrap_resolvers = [
+          "1.1.1.1:53"
+          "[2606:4700:4700::1111]:53"
+          "8.8.8.8:53"
+          "[2001:4860:4860::8888]:53"
+        ];
         ipv6_servers = true;
         server_names = [
           "cloudflare"
