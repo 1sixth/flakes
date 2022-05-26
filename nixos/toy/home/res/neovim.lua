01f62c30898b1b890d0bc91016daf2f1d5946376
1sixth <1sixth@shinta.ro>
2022-05-26 11:42:04 +0800

    toy: neovim: enable relative number

--- nixos/toy/home/res/neovim.lua
+++ nixos/toy/home/res/neovim.lua
@@ -2,6 +2,7 @@ vim.opt.background = 'light'
 vim.opt.expandtab = true
 vim.opt.ignorecase = true
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
 vim.opt.showmode = false
 vim.opt.smartcase = true
