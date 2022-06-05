fd122ab2ee6b1ae82f35845a1f7235e22c088fcd
1sixth <1sixth@shinta.ro>
2022-06-05 12:38:29 +0800

    toy: mpv config cleanup

--- nixos/toy/home/mpv.nix
+++ nixos/toy/home/mpv.nix
@@ -14,16 +14,12 @@
 
       "Shift+RIGHT" = "no-osd seek  1 exact"; # seek exactly 1 second forward
       "Shift+LEFT" = "no-osd seek -1 exact"; # seek exactly 1 second backward
-      "Shift+UP" = "no-osd seek  5 exact"; # seek exactly 5 seconds forward
-      "Shift+DOWN" = "no-osd seek -5 exact"; # seek exactly 5 seconds backward
-
 
       "Ctrl+RIGHT" = "add chapter 1 "; # seek to the next chapter
       "Ctrl+LEFT" = "add chapter -1"; # seek to the previous chapter
 
       "Ctrl+Shift+RIGHT" = "playlist-next"; # skip to the next file
       "Ctrl+Shift+LEFT" = "playlist-prev"; # skip to the previous file
-
 
       "[" = "multiply speed 1/1.1"; # decrease the playback speed
       "]" = "multiply speed 1.1"; # increase the playback speed
@@ -38,6 +34,7 @@
       SPACE = "cycle pause"; # toggle pause/playback mode
 
       i = "script-binding stats/display-stats-toggle"; # toggle displaying information and statistics
+
       "`" = "script-binding console/enable"; # open the console
 
       z = "add sub-delay +0.1"; # shift subtitles 100 ms earlier
@@ -45,18 +42,20 @@
 
       "9" = "add volume -5";
       "0" = "add volume 5";
+
       m = "cycle mute"; # toggle mute
 
       v = "cycle sub-visibility"; # hide or show the subtitles
 
       h = "cycle audio"; # switch audio track
       H = "cycle audio down"; # switch audio track backwards
+
       j = "cycle sub"; # switch subtitle track
       J = "cycle sub down"; # switch subtitle track backwards
 
       f = "cycle fullscreen"; # toggle fullscreen
 
-      L = ''cycle-values loop-file "inf" "no"''; # toggle infinite looping
+      l = ''cycle-values loop-file "inf" "no"''; # toggle infinite looping
 
       POWER = "quit";
       PLAY = "cycle pause"; # toggle pause/playback mode
@@ -75,28 +74,24 @@
       CLOSE_WIN = "quit";
 
       "Ctrl+h" = "cycle-values hwdec auto-safe no"; # toggle hardware decoding
+
       F8 = "show-text \${playlist}"; # show the playlist
       F9 = "show-text \${track-list}"; # show the list of video, audio and sub tracks
     };
     config = {
-      fs = "yes";
-      keep-open = "yes";
-      profile = "gpu-hq";
-      video-sync = "display-resample";
-      slang = "en,eng,zh-Hans,zh-Hant,zh-CN,zh-TW,zh-HK,zh-SG,zho,chi";
-      sub-auto = "fuzzy";
-
-      # From VSB Studio (https://vcb-s.com/archives/7594)
+      blend-subtitles = "video";
+      fullscreen = "";
+      gpu-api = "vulkan";
       # icc-profile-auto not implemented on Wayland
       #icc-profile-auto = "";
-      blend-subtitles = "video";
       interpolation = "";
-      tscale = "oversample";
-
-      # From cczzhh (https://bbs.vcb-s.com/thread-2730-1-1.html)
-      gpu-api = "vulkan";
-
+      keep-open = "always";
       no-input-default-bindings = "";
+      profile = "gpu-hq";
+      slang = "en-orig,zh-CN,zh-TW,zh-HK,zh-SG,zh-Hans,zh-Hant,chi,zho";
+      sub-auto = "fuzzy";
+      tscale = "oversample";
+      video-sync = "display-resample";
     };
     enable = true;
     scripts = with pkgs.mpvScripts; [
