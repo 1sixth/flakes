663fd89afeb57b43a7719a08ae22b576a4657568
1sixth <1sixth@shinta.ro>
2022-06-20 08:21:34 +0800

    tyo5: delete leftover open-iscsi configs

--- nixos/tyo5/configuration.nix
+++ nixos/tyo5/configuration.nix
@@ -18,11 +18,6 @@
 
   networking.hostName = "tyo5";
 
-  services.openiscsi = {
-    enable = true;
-    name = "iqn.2015-02.oracle.boot:uefi";
-  };
-
   sops.defaultSopsFile = ./secrets.yaml;
 
   system.stateVersion = "22.05";
