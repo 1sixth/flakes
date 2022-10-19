6ac53d9f9a6151eade3e23c820c1e33cbf596d90
1sixth <1sixth@shinta.ro>
2022-10-19 13:10:29 +0800

    toy: chromium: add libredirect

--- nixos/toy/home/browser.nix
+++ nixos/toy/home/browser.nix
@@ -9,9 +9,15 @@
       "--proxy-server=socks5://127.0.0.1:1080"
     ];
     enable = true;
-    extensions = [{
-      id = "dmmjlmbkigbgpnjfiimhlnbnmppjhpea";
-      updateUrl = "https://raw.githubusercontent.com/ronggang/PT-Plugin-Plus/master/update/index.xml";
-    }];
+    extensions = [
+      {
+        id = "dmmjlmbkigbgpnjfiimhlnbnmppjhpea";
+        updateUrl = "https://raw.githubusercontent.com/ronggang/PT-Plugin-Plus/master/update/index.xml";
+      }
+      {
+        id= "oladmjdebphlnjjcnomfhhbfdldiimaf";
+        updateUrl = "https://raw.githubusercontent.com/libredirect/libredirect/master/src/updates/updates.xml";
+      }
+    ];
   };
 }
