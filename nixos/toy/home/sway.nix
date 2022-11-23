b69e79a542154c58902df7c5e5058ef458810341
1sixth <1sixth@shinta.ro>
2022-11-23 12:23:35 +0800

    toy: sway: fix thunderbird assignment

--- nixos/toy/home/sway.nix
+++ nixos/toy/home/sway.nix
@@ -6,7 +6,7 @@
       assigns = {
         "2" = [{ app_id = "firefox"; } { app_id = "chromium"; } { class = "Chromium-browser"; }];
         "3" = [{ app_id = "mpv"; }];
-        "4" = [{ class = "thunderbird"; }];
+        "4" = [{ app_id = "thunderbird"; }];
       };
       bars = [ ];
       gaps = {
