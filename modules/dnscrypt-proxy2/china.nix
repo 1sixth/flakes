5b850913a918d76812c0961db2ba05068b43e0df
1sixth <1sixth@shinta.ro>
2022-12-23 22:44:55 +0800

    modules.dnscrypt-proxy2.china: use custom stamps
    add IPv6 bootstrap resolvers

--- modules/dnscrypt-proxy2/china.nix
+++ modules/dnscrypt-proxy2/china.nix
@@ -7,14 +7,12 @@
     dnscrypt-proxy2 = {
       enable = true;
       settings = {
-        bootstrap_resolvers = [ "223.5.5.5:53" "119.29.29.29:53" ];
+        bootstrap_resolvers = [ "223.5.5.5:53" "119.29.29.29:53" "[2400:3200::1]:53" "[2402:4e00::]:53" ];
         ipv6_servers = true;
         server_names = [ "alidns-doh" "dnspod-doh" ];
-        # A manual update of stamps may be necessary in the furure,
-        # see https://github.com/DNSCrypt/dnscrypt-resolvers/pull/602.
         static = {
-          alidns-doh.stamp = "sdns://AgAAAAAAAAAACTIyMy41LjUuNSAUZf-XFWhwvjDwNPWQzx8E3VDwpSDoT4pSfpwaLofrgA5kbnMuYWxpZG5zLmNvbQovZG5zLXF1ZXJ5";
-          dnspod-doh.stamp = "sdns://AgAAAAAAAAAAACDBlAfoWsQD52fP2oOBh_Ag-lY6yBaIr1EMIqd559RaVgdkb2gucHViCi9kbnMtcXVlcnk";
+          alidns-doh.stamp = "sdns://AgAAAAAAAAAAAAAOZG5zLmFsaWRucy5jb20KL2Rucy1xdWVyeQ";
+          dnspod-doh.stamp = "sdns://AgAAAAAAAAAAAAAHZG9oLnB1YgovZG5zLXF1ZXJ5";
         };
       };
       upstreamDefaults = false;
