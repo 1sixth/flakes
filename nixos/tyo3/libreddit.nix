6f32879457661221931c2dec4f2d87a44ee5090f
1sixth <1sixth@shinta.ro>
2023-01-30 09:32:51 +0800

    tyo3: libreddit: new line

--- nixos/tyo3/libreddit.nix
+++ nixos/tyo3/libreddit.nix
@@ -13,7 +13,9 @@
           rule = "Host(`reddit.shinta.ro`)";
           service = "libreddit";
         };
-        services.libreddit.loadBalancer.servers = [{ url = "http://${config.services.libreddit.address}:${builtins.toString config.services.libreddit.port}"; }];
+        services.libreddit.loadBalancer.servers = [{
+          url = "http://${config.services.libreddit.address}:${builtins.toString config.services.libreddit.port}";
+          }];
       };
     };
   };
