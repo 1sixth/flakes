4b63705e89dc5b9809be8bbf54e44736c7cb1599
1sixth <1sixth@shinta.ro>
2023-01-30 11:54:23 +0800

    tyo0: vaultwarden: make use of ensureDatabases and ensureUsers

--- nixos/tyo0/vaultwarden.nix
+++ nixos/tyo0/vaultwarden.nix
@@ -2,18 +2,24 @@
 
 {
   services = {
+    postgresql = {
+      ensureDatabases = [ "vaultwarden" ];
+      ensureUsers = [{
+        name = "vaultwarden";
+        ensurePermissions = {
+          "DATABASE vaultwarden" = "ALL PRIVILEGES";
+        };
+      }];
+    };
     traefik.dynamicConfigOptions.http = {
       routers.vaultwarden = {
         rule = "Host(`vault.shinta.ro`)";
         service = "vaultwarden";
       };
-      services.vaultwarden.loadBalancer.servers = [{ url = "http://127.0.0.1:8000"; }];
+      services.vaultwarden.loadBalancer.servers = [{ url = "http://127.0.0.1:8001"; }];
     };
     vaultwarden = {
       config.dataFolder = "/var/lib/vaultwarden";
-      # TODO: preStart:
-      # createuser vaultwarden
-      # createdb -O vaultwarden vaultwarden
       dbBackend = "postgresql";
       enable = true;
       environmentFile = config.sops.secrets.vaultwarden.path;
