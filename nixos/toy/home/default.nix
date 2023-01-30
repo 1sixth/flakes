e6714b3f82486a06ac607c6b36f87e7925cc71c8
1sixth <1sixth@shinta.ro>
2023-01-30 09:29:09 +0800

    toy: switch back to swaylock-effects

--- nixos/toy/home/default.nix
+++ nixos/toy/home/default.nix
@@ -6,13 +6,6 @@ let
     url = "https://i.pximg.net/img-original/img/2017/04/21/18/35/04/62506385_p0.jpg";
     sha256 = "sha256-B1/J9ReWB0AD2pDxVsRcJ/naFOX+Tx4gDihUp1QLsvA=";
   };
-  wallpaper-blurred = pkgs.runCommand "wallpaper-blurred.png"
-    {
-      nativeBuildInputs = with pkgs; [ imagemagick ];
-    }
-    ''
-      convert -blur 7x5 ${wallpaper} $out
-    '';
 in
 
 {
@@ -200,8 +193,12 @@ in
       settings.add_newline = false;
     };
     swaylock.settings = {
+      clock = true;
       daemonize = true;
-      image = wallpaper-blurred.outPath;
+      effect-blur = "7x5";
+      effect-vignette = "0.5:0.5";
+      grace = 3;
+      image = wallpaper.outPath;
       indicator-caps-lock = true;
       scaling = "fill";
       show-failed-attempts = true;
@@ -231,7 +228,7 @@ in
     swayidle = {
       enable = true;
       events = [{
-        command = "${pkgs.swaylock}/bin/swaylock";
+        command = "${pkgs.swaylock-effects}/bin/swaylock";
         event = "lock";
       }];
       timeouts = [
