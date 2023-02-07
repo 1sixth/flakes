c06160a3ddad48c475b80f2dcf758b3511a48e71
1sixth <1sixth@shinta.ro>
2023-02-07 19:16:27 +0800

    toy: use firefox-esr; utilize `SearchEngines` policy

--- nixos/toy/home/browser.nix
+++ nixos/toy/home/browser.nix
@@ -3,7 +3,7 @@
 {
   programs.firefox = {
     enable = true;
-    package = pkgs.firefox.override {
+    package = pkgs.firefox-esr.override {
       extraPolicies = {
         DisableAppUpdate = true;
         DisableFirefoxStudies = true;
@@ -46,6 +46,67 @@
           SOCKSProxy = "127.0.0.1:1080";
           SOCKSVersion = 5;
           UseProxyForDNS = true;
+        };
+        SearchEngines = {
+          Add = [
+            {
+              Name = "Google Search";
+              URLTemplate = "https://www.google.com/search?q={searchTerms}";
+              IconURL = "https://www.google.com/favicon.ico";
+              Alias = "gg";
+            }
+            {
+              Name = "GitHub";
+              URLTemplate = "https://github.com/search?q={searchTerms}&type=repositories";
+              IconURL = "https://github.com/favicon.ico";
+              Alias = "g";
+            }
+            {
+              Name = "YouTube";
+              URLTemplate = "https://www.youtube.com/results?search_query={searchTerms}";
+              IconURL = "https://www.youtube.com/favicon.ico";
+              Alias = "y";
+            }
+            {
+              Name = "维基百科 (zh)";
+              URLTemplate = "https://zh.wikipedia.org/w/index.php?title=Special:%E6%90%9C%E7%B4%A2&search={searchTerms}";
+              IconURL = "https://zh.wikipedia.org/favicon.ico";
+              Alias = "w";
+            }
+            {
+              Name = "维基百科 (en)";
+              URLTemplate = "https://en.wikipedia.org/w/index.php?title=Special:Search&search={searchTerms}";
+              IconURL = "https://en.wikipedia.org/favicon.ico";
+              Alias = "we";
+            }
+            {
+              Name = "ArchWiki (en)";
+              URLTemplate = "https://wiki.archlinux.org/index.php?title=Special:Search&search={searchTerms}";
+              IconURL = "https://wiki.archlinux.org/favicon.ico";
+              Alias = "arch";
+            }
+            {
+              Name = "Arch manual pages";
+              URLTemplate = "https://man.archlinux.org/search?q={searchTerms}&go=Go";
+              IconURL = "https://man.archlinux.org/favicon.ico";
+              Alias = "man";
+            }
+            {
+              Name = "萌娘百科";
+              URLTemplate = "https://zh.moegirl.org.cn/index.php?title=Special:%E6%90%9C%E7%B4%A2&search={searchTerms}";
+              IconURL = "https://zh.moegirl.org.cn/favicon.ico";
+              Alias = "moe";
+            }
+          ];
+          Default = "Google Search";
+          Remove = [
+            # Google won't be removed for some mysterious reason.
+            "Google"
+            "Amazon.com"
+            "Bing"
+            "DuckDuckGo"
+            "Wikipedia (en)"
+          ];
         };
         SanitizeOnShutdown = {
           Cache = true;
