19743b2b91081c1cbb4dab8c486577e49c0746c1
1sixth <1sixth@shinta.ro>
2023-02-09 17:28:54 +0800

    modules.v2ray: remove

--- modules/default.nix
+++ modules/default.nix
@@ -1,6 +1,4 @@
 {
-  # TODO: Declare that `v2ray.server` and `qbittorrent-nox`
-  # depend on `server`.
   dnscrypt-proxy2 = {
     china = import ./dnscrypt-proxy2/china.nix;
     earth = import ./dnscrypt-proxy2/earth.nix;
@@ -13,8 +11,4 @@
     server = import ./sing-box/server.nix;
   };
   stress-ng = import ./stress-ng.nix;
-  v2ray = {
-    server = import ./v2ray/server.nix;
-    client = import ./v2ray/client.nix;
-  };
 }
