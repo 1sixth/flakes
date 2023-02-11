56017f62f499a1e92f31e73b4e0441c7286baa50
1sixth <1sixth@shinta.ro>
2023-02-11 22:25:29 +0800

    modules.sing-box: update sing-box.json

--- modules/sing-box/server.nix
+++ modules/sing-box/server.nix
@@ -1,16 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  services.traefik.dynamicConfigOptions.http = {
-    routers.sing-box = {
-      rule = "Host(`${config.networking.hostName}.9875321.xyz`, `${config.networking.hostName}-cf.9875321.xyz`) && Path(`/websocket`)";
-      service = "sing-box";
-    };
-    services.sing-box.loadBalancer.servers = [{
-      url = "http://127.0.0.1:10000";
-    }];
-  };
-
   sops.secrets."sing-box.json" = {
     restartUnits = [ "sing-box.service" ];
     sopsFile = ./secrets.yaml;
