bb3438cdf6948ba0f5ed0b24ccedc5933ad80ec1
1sixth <1sixth@shinta.ro>
2023-04-03 14:50:35 +0800

    nrt0, nrt1: remove qbittorrent-nox

--- nixos/nrt0/default.nix
+++ nixos/nrt0/default.nix
@@ -7,7 +7,6 @@ nixpkgs.lib.nixosSystem {
     inputs.sops-nix.nixosModules.sops
     self.nixosModules.base
     self.nixosModules.dnscrypt-proxy2.earth
-    self.nixosModules.qbittorrent-nox
     self.nixosModules.server
     self.nixosModules.stress-ng
     self.nixosModules.v2ray.server
