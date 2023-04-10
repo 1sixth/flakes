32cb4c84eb1a63f5de1aab4c1d3d79f5d55f77f0
1sixth <1sixth@shinta.ro>
2023-04-11 01:21:31 +0800

    toy: fully power down discrete GPU
    https://wiki.archlinux.org/title/Hybrid_graphics#Fully_power_down_discrete_GPU

--- nixos/toy/configuration.nix
+++ nixos/toy/configuration.nix
@@ -5,6 +5,7 @@
 
   boot = {
     binfmt.emulatedSystems = [ "aarch64-linux" ];
+    blacklistedKernelModules = [ "nouveau" ];
     kernelParams = [ "amd_pstate=passive" ];
     supportedFilesystems = [ "ntfs" ];
   };
@@ -134,6 +135,19 @@
       pulse.enable = true;
     };
     tlp.enable = true;
+    udev.extraRules = ''
+      # Remove NVIDIA USB xHCI Host Controller devices, if present
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+
+      # Remove NVIDIA USB Type-C UCSI devices, if present
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+
+      # Remove NVIDIA Audio devices, if present
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+
+      # Remove NVIDIA VGA/3D controller devices
+      ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+    '';
     upower.enable = true;
   };
 
