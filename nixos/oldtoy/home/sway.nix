2e7edc06f4d15210ef6bf5b23ecec668bb27c89e
1sixth <1sixth@shinta.ro>
2023-06-13 13:06:53 +0800

    oldtoy: sway: unset bindswitch
    It's convenient to close the lid, and have all the workspaces
    and windows move to the external display. But this happens to
    conflict with swaylock-effects's `--screenshots` feature, where
    if you close the lid and let swayidle kick in, swaylock-effects
    would capture a white blank screen.

--- nixos/oldtoy/home/sway.nix
+++ nixos/oldtoy/home/sway.nix
@@ -71,10 +71,6 @@ in
       terminal = "foot";
     };
     enable = true;
-    extraConfig = ''
-      bindswitch --locked --reload lid:on output eDP-1 disable
-      bindswitch --locked --reload lid:off output eDP-1 enable
-    '';
     package = null;
   };
 }
