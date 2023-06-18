0813a86b5afd5b751d1cdc26009c2c4dd5bbb040
1sixth <1sixth@shinta.ro>
2023-06-18 18:58:06 +0800

    laptop0: neovim: add all treesitter grammars
    It increases the closure size by ~140MB.

--- nixos/laptop0/home/neovim.nix
+++ nixos/laptop0/home/neovim.nix
@@ -8,27 +8,10 @@
       :source ${./res/neovim.lua}
     '';
     plugins = with pkgs.vimPlugins; [
-      (nvim-treesitter.withPlugins (
-          plugins: with plugins; [
-            tree-sitter-bash
-            tree-sitter-c
-            tree-sitter-cmake
-            tree-sitter-cpp
-            tree-sitter-fish
-            tree-sitter-html
-            tree-sitter-lua
-            tree-sitter-json
-            tree-sitter-markdown
-            tree-sitter-nix
-            tree-sitter-python
-            tree-sitter-rust
-            tree-sitter-toml
-            tree-sitter-yaml
-          ]
-      ))
       editorconfig-nvim
       lualine-nvim
       nvim-lastplace
+      nvim-treesitter.withAllGrammars
       papercolor-theme
       which-key-nvim
     ];
