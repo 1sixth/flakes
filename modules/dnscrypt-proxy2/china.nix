6f5cf21784214ff01632b22e8a59239afee68e18
1sixth <1sixth@shinta.ro>
2023-06-26 12:17:29 +0800

    modules.dnscrypt-proxy2.china: lint

--- modules/dnscrypt-proxy2/china.nix
+++ modules/dnscrypt-proxy2/china.nix
@@ -10,9 +10,17 @@
     dnscrypt-proxy2 = {
       enable = true;
       settings = {
-        bootstrap_resolvers = [ "223.5.5.5:53" "119.29.29.29:53" "[2400:3200::1]:53" "[2402:4e00::]:53" ];
+        bootstrap_resolvers = [
+          "223.5.5.5:53"
+          "119.29.29.29:53"
+          "[2400:3200::1]:53"
+          "[2402:4e00::]:53"
+        ];
         ipv6_servers = true;
-        server_names = [ "alidns-doh" "dnspod-doh" ];
+        server_names = [
+          "alidns-doh"
+          "dnspod-doh"
+        ];
         static = {
           alidns-doh.stamp = "sdns://AgAAAAAAAAAAAAAOZG5zLmFsaWRucy5jb20KL2Rucy1xdWVyeQ";
           dnspod-doh.stamp = "sdns://AgAAAAAAAAAAAAAHZG9oLnB1YgovZG5zLXF1ZXJ5";
