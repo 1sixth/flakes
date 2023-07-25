a28d9a9d8dceefe8fc2297c38a803ed342e527ca
1sixth <1sixth@shinta.ro>
2023-07-25 20:04:47 +0800

    Revert "nrt0, nrt1, phx0: patch systemd to get DHCPv6 working"
    This reverts commit b12ef2380abbe177cf2f0bee6d9de5fd57131887.
    
    This patch is no longer necessary since 253.6.

--- nixos/nrt1/configuration.nix
+++ nixos/nrt1/configuration.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   imports = [ ./hardware.nix ];
@@ -6,13 +6,4 @@
   networking.hostName = "nrt1";
 
   sops.defaultSopsFile = ./secrets.yaml;
-
-  systemd.package = pkgs.systemd.overrideAttrs (old: {
-    patches = (old.patches or [ ]) ++ [
-      (pkgs.fetchpatch {
-        url = "https://github.com/systemd/systemd/commit/1d2b93ff89f28b18c222321c8c7bc48be2185375.patch";
-        hash = "sha256-4hxgKra3Mvszkxxhwc7XgucGDjI0VhfLno1w++tlHYo=";
-      })
-    ];
-  });
 }
