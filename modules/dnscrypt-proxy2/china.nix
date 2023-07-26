c29518293d083d311791ea5fe8b1576de6e8bff4
1sixth <1sixth@shinta.ro>
2023-07-26 14:19:32 +0800

    modules.dnscrypt-proxy2: update bootstrap_resolvers

--- modules/dnscrypt-proxy2/china.nix
+++ modules/dnscrypt-proxy2/china.nix
@@ -11,16 +11,19 @@
       enable = true;
       settings = {
         bootstrap_resolvers = [
-          "223.5.5.5:53"
-          "119.29.29.29:53"
           "[2400:3200::1]:53"
+          "[2400:3200:baba::1]:53"
           "[2402:4e00::]:53"
+          "223.5.5.5:53"
+          "223.6.6.6:53"
+          "119.29.29.29:53"
         ];
         ipv6_servers = true;
         server_names = [
           "alidns-doh"
           "dnspod-doh"
         ];
+        # https://dnscrypt.info/stamps/
         static = {
           alidns-doh.stamp = "sdns://AgAAAAAAAAAAAAAOZG5zLmFsaWRucy5jb20KL2Rucy1xdWVyeQ";
           dnspod-doh.stamp = "sdns://AgAAAAAAAAAAAAAHZG9oLnB1YgovZG5zLXF1ZXJ5";
