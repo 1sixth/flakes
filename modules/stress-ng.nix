a7eba9d91de814ea066bdb9613f5e54fcbd87900
1sixth <1sixth@shinta.ro>
2023-08-28 01:24:26 +0800

    modules.stress-ng: keep 15% memory usage instead

--- modules/stress-ng.nix
+++ modules/stress-ng.nix
@@ -2,12 +2,13 @@
 
 {
   systemd.services.stress-ng = {
-    description = "Keep 10% CPU Usage";
+    description = "Keep 15% Memory Usage";
     serviceConfig = {
       DynamicUser = true;
-      ExecStart = "${pkgs.stress-ng}/bin/stress-ng --cpu 0 --cpu-load 10 --timeout 0";
-      Nice = 19;
+      ExecStart = "${pkgs.stress-ng}/bin/stress-ng --timeout 0 --vm 1 --vm-bytes 15% --vm-hang 0 --vm-locked";
+      LimitMEMLOCK = "infinity";
       StateDirectory = "stress-ng";
+      Type = "exec";
       WorkingDirectory = "/var/lib/stress-ng/";
     };
     wantedBy = [ "multi-user.target" ];
