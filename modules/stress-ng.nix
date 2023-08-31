efe57c97ef8e0f37e0d2be801b7b8067e83bf01a
1sixth <1sixth@shinta.ro>
2023-08-31 14:29:46 +0800

    modules.stress-ng: fix `--timeout` bug
    Another workaround is to use something like `--timeout 999y`.

--- modules/stress-ng.nix
+++ modules/stress-ng.nix
@@ -1,11 +1,23 @@
 { pkgs, ... }:
 
+let
+  stress-ng = pkgs.stress-ng.overrideAttrs (old: {
+    patches = (old.patches or [ ]) ++ [
+      (pkgs.fetchpatch {
+        url = "https://github.com/ColinIanKing/stress-ng/commit/39d337dbae343dcd198e26c2d83894a7f15ec4b8.patch";
+        hash = "sha256-y8HvqR1ja3KvyezjZTfTeBGxvFoPs/w47qRLm0DnU08=";
+        revert = true;
+      })
+    ];
+  });
+in
+
 {
   systemd.services.stress-ng = {
     description = "Keep 15% Memory Usage";
     serviceConfig = {
       DynamicUser = true;
-      ExecStart = "${pkgs.stress-ng}/bin/stress-ng --timeout 0 --vm 1 --vm-bytes 15% --vm-hang 0 --vm-locked";
+      ExecStart = "${stress-ng}/bin/stress-ng --timeout 0 --vm 1 --vm-bytes 15% --vm-hang 0 --vm-locked";
       LimitMEMLOCK = "infinity";
       StateDirectory = "stress-ng";
       Type = "exec";
