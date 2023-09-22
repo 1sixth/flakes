6de20f4f86e39be47e8503601963bd222a7ae3ac
1sixth <1sixth@shinta.ro>
2023-09-22 09:11:50 +0800

    modules.server: let sshd listen on 2222 as well

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -60,6 +60,10 @@
         path = "/etc/ssh/ssh_host_ed25519_key";
         type = "ed25519";
       }];
+      ports = [
+        22
+        2222
+      ];
       settings = {
         KbdInteractiveAuthentication = false;
         PasswordAuthentication = false;
