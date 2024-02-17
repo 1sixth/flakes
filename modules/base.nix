da1e5b7d3904fd8512e67a57481396472fd6b733
1sixth <1sixth@shinta.ro>
2024-02-17 14:10:55 +0800

    modules.base: add ssh knownHosts for github and gitlab

--- modules/base.nix
+++ modules/base.nix
@@ -80,6 +80,10 @@
     iotop.enable = true;
     mtr.enable = true;
     nano.enable = false;
+    ssh.knownHosts = {
+      "github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+      "gitlab.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
+    };
     starship = {
       enable = true;
       settings.add_newline = false;
