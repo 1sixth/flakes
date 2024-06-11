77723760e450bcead7dd7651543b79d13a31e2e3
1sixth <1sixth@shinta.ro>
2024-06-11 15:14:39 +0800

    laptop0: mpv: declare scripts in another way

--- nixos/laptop0/home/mpv.nix
+++ nixos/laptop0/home/mpv.nix
@@ -103,10 +103,14 @@
       ytdl-raw-options = "yes-playlist=";
     };
     enable = true;
-    scripts = with pkgs.mpvScripts; [
-      autoload
-      mpris
-      sponsorblock
-    ];
+    package = (
+      pkgs.mpv.override {
+        scripts = with pkgs.mpvScripts; [
+          autoload
+          mpris
+          sponsorblock
+        ];
+      }
+    );
   };
 }
