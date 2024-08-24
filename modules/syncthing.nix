9b8ca882a919a4bd9ce36bf15ea969ab07bc4c7e
1sixth <1sixth@shinta.ro>
2024-08-25 01:02:45 +0800

    modules.syncthing: do not set low priority

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -9,7 +9,10 @@
       settings = {
         # https://docs.syncthing.net/users/faq.html#why-do-i-get-host-check-error-in-the-gui-api
         gui.insecureSkipHostcheck = true;
-        options.urAccepted = -1;
+        options = {
+          setLowPriority = false;
+          urAccepted = -1;
+        };
       };
     };
     traefik.dynamicConfigOptions.http = {
