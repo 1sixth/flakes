e2247781912fc8456d0eeebcc2bc8abcea88b560
1sixth <1sixth@shinta.ro>
2024-08-24 15:53:03 +0800

    modules.base: keep 20% cpu usage instead

--- modules/stress-ng.nix
+++ modules/stress-ng.nix
@@ -2,13 +2,12 @@
 
 {
   systemd.services.stress-ng = {
-    description = "Keep 15% Memory Usage";
+    description = "Keep 20% CPU Usage";
     serviceConfig = {
       DynamicUser = true;
-      ExecStart = "${pkgs.stress-ng}/bin/stress-ng --timeout 0 --vm 1 --vm-bytes 15% --vm-hang 0 --vm-locked";
-      LimitMEMLOCK = "infinity";
+      ExecStart = "${pkgs.stress-ng}/bin/stress-ng  --cpu 0 --cpu-load 20 --timeout 0";
+      Nice = 19;
       StateDirectory = "stress-ng";
-      Type = "exec";
       WorkingDirectory = "/var/lib/stress-ng";
     };
     wantedBy = [ "multi-user.target" ];
