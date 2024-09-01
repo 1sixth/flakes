92132d3a227328278d16aa93279514a41a4ee049
1sixth <1sixth@shinta.ro>
2024-09-01 12:53:45 +0800

    home: hypridle: tweak timeouts

--- home-manager/hypridle.nix
+++ home-manager/hypridle.nix
@@ -5,15 +5,18 @@
     enable = true;
     settings = {
       general.lock_cmd = "hyprlock";
+      # This is probably a bug, see hypridle/issues/86 for more details.
       listener = [
+        # lock the screen after 1 minute
         {
-          on-timeout = "hyprlock";
-          timeout = 300;
+          on-timeout = "loginctl lock-session";
+          timeout = 60;
         }
+        # then turn the screen off after 2 minutes
         {
           on-resume = "hyprctl dispatch dpms on";
           on-timeout = "hyprctl dispatch dpms off";
-          timeout = 305;
+          timeout = 120;
         }
       ];
     };
