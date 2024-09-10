4f77d5705569a975c4b23252035ecc0f78aa46cc
1sixth <1sixth@shinta.ro>
2024-09-10 16:13:20 +0800

    modules.server: remove vnstat

--- modules/nixos/server/default.nix
+++ modules/nixos/server/default.nix
@@ -48,25 +48,22 @@
 
   security.sudo.enable = false;
 
-  services = {
-    openssh = {
-      enable = true;
-      hostKeys = [
-        {
-          path = "/etc/ssh/ssh_host_ed25519_key";
-          type = "ed25519";
-        }
-      ];
-      ports = [
-        22
-        2222
-      ];
-      settings = {
-        KbdInteractiveAuthentication = false;
-        PasswordAuthentication = false;
-      };
+  services.openssh = {
+    enable = true;
+    hostKeys = [
+      {
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+    ];
+    ports = [
+      22
+      2222
+    ];
+    settings = {
+      KbdInteractiveAuthentication = false;
+      PasswordAuthentication = false;
     };
-    vnstat.enable = true;
   };
 
   sops = {
