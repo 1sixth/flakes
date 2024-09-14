b7b02dbdda151e7894de8cec58862f91d95033ed
1sixth <1sixth@shinta.ro>
2024-09-15 00:37:52 +0800

    modules.laptop: add patch for impala

--- modules/nixos/laptop/overlays.nix
+++ modules/nixos/laptop/overlays.nix
@@ -11,6 +11,31 @@
         vscode-marketplace
         ;
 
+      impala = prev.impala.overrideAttrs (old: {
+        patches = (old.patches or [ ]) ++ [
+          (prev.writeText "impala.patch" ''
+            diff --git a/src/app.rs b/src/app.rs
+            index 8d245f0..742c9ef 100644
+            --- a/src/app.rs
+            +++ b/src/app.rs
+            @@ -127,11 +127,7 @@ impl App {
+
+                     let agent_manager = session.register_agent(agent).await?;
+
+            -        let color_mode = match terminal_light::luma() {
+            -            Ok(luma) if luma > 0.6 => ColorMode::Light,
+            -            Ok(_) => ColorMode::Dark,
+            -            Err(_) => ColorMode::Dark,
+            -        };
+            +        let color_mode = ColorMode::Dark;
+
+                     Ok(Self {
+                         running: true,
+
+          '')
+        ];
+      });
+
       telegram-desktop = prev.telegram-desktop.overrideAttrs (old: {
         patches = (old.patches or [ ]) ++ [
           (prev.fetchpatch {
