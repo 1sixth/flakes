70fd5e759736dcb793865301b2fa00bd9bb1af6a
1sixth <1sixth@shinta.ro>
2024-09-27 14:19:11 +0800

    laptop1: set hosts-file

--- nixos/laptop1/configuration.nix
+++ nixos/laptop1/configuration.nix
@@ -18,10 +18,13 @@
 
   networking.hostName = "laptop1";
 
-  services.logind = {
-    powerKey = "ignore";
-    suspendKey = "ignore";
-    suspendKeyLongPress = "ignore";
+  services = {
+    logind = {
+      powerKey = "ignore";
+      suspendKey = "ignore";
+      suspendKeyLongPress = "ignore";
+    };
+    smartdns.settings.hosts-file = config.sops.secrets.hosts.path;
   };
 
   sops = {
@@ -31,6 +34,7 @@
         owner = "systemd-network";
         path = "/etc/systemd/network/company.network";
       };
+      hosts.mode = "0444";
       password_root.neededForUsers = true;
       password_one6th.neededForUsers = true;
       u2f_keys.mode = "0444";
