6a4b590bee0264028523a3570436877f261dbf2b
1sixth <1sixth@shinta.ro>
2024-09-30 10:42:59 +0800

    home.hypridle: tweak settings
    also remove comments because the bug has been fixed

--- modules/home/hypridle.nix
+++ modules/home/hypridle.nix
@@ -8,18 +8,15 @@
         before_sleep_cmd = "loginctl lock-session";
         lock_cmd = "hyprlock";
       };
-      # This is probably a bug, see hypridle/issues/86 for more details.
       listener = [
-        # lock the screen after 5 minute
         {
           on-timeout = "loginctl lock-session";
-          timeout = 300;
+          timeout = 120;
         }
-        # then turn the screen off after 5 minutes and 5 seconds
         {
           on-resume = "hyprctl dispatch dpms on";
           on-timeout = "hyprctl dispatch dpms off";
-          timeout = 305;
+          timeout = 300;
         }
       ];
     };
