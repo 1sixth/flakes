9f209e92696d2f9e957f1c4db87754b134b32305
1sixth <1sixth@shinta.ro>
2024-10-15 15:55:56 +0800

    modules.dns: change response-mode

--- modules/nixos/dns.nix
+++ modules/nixos/dns.nix
@@ -76,6 +76,7 @@ in
         no-daemon = true;
         no-pidfile = true;
         prefetch-domain = true;
+        response-mode = "fastest-ip";
         server = Bootstrap ++ MagicDNS;
         server-https = DoH;
       };
