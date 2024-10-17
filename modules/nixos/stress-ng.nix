3f516f0bb2614a04e5ddda8591a07b3cbeda1c7c
1sixth <1sixth@shinta.ro>
2024-10-17 11:36:33 +0800

    modules.stress-ng: keep 25% memory usage

--- modules/nixos/stress-ng.nix
+++ modules/nixos/stress-ng.nix
@@ -1,12 +1,13 @@
 { pkgs, ... }:
 
 {
+  # https://docs.oracle.com/en-us/iaas/Content/FreeTier/freetier_topic-Always_Free_Resources.htm
   systemd.services.stress-ng = {
-    description = "Keep 20% CPU Usage";
+    description = "Keep 25% Memory Usage";
     serviceConfig = {
       DynamicUser = true;
-      ExecStart = "${pkgs.stress-ng}/bin/stress-ng  --cpu 0 --cpu-load 20 --timeout 0";
-      Nice = 19;
+      ExecStart = "${pkgs.stress-ng}/bin/stress-ng  --timeout 0 --vm 1 --vm-bytes 25% --vm-hang 0 --vm-locked";
+      LimitMEMLOCK = "infinity";
       StateDirectory = "stress-ng";
       WorkingDirectory = "/var/lib/stress-ng";
     };
