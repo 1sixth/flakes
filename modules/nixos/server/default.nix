41ba8730d0894d9c2832041d833508c180aa2d26
1sixth <1sixth@shinta.ro>
2024-10-19 11:35:13 +0800

    modules.server: enable fail2ban

--- modules/nixos/server/default.nix
+++ modules/nixos/server/default.nix
@@ -58,21 +58,24 @@
 
   security.sudo.enable = false;
 
-  services.openssh = {
-    enable = true;
-    hostKeys = [
-      {
-        path = "/etc/ssh/ssh_host_ed25519_key";
-        type = "ed25519";
-      }
-    ];
-    ports = [
-      22
-      2222
-    ];
-    settings = {
-      KbdInteractiveAuthentication = false;
-      PasswordAuthentication = false;
+  services = {
+    fail2ban.enable = true;
+    openssh = {
+      enable = true;
+      hostKeys = [
+        {
+          path = "/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+      ];
+      ports = [
+        22
+        2222
+      ];
+      settings = {
+        KbdInteractiveAuthentication = false;
+        PasswordAuthentication = false;
+      };
     };
   };
 
