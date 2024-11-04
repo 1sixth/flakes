b6425d9789b647dc7515e2df056ce3c95955a4d3
1sixth <1sixth@shinta.ro>
2024-11-04 19:23:31 +0800

    Revert "laptop1: remove"
    This reverts commit 2364833c9fd9a659c5bef19f2d08766467eefe2d.

---
+++ nixos/laptop1/configuration.nix
@@ -0,0 +1,48 @@
+{ config, ... }:
+
+{
+  imports = [ ./hardware.nix ];
+
+  deployment.tags = [ "china" ];
+
+  environment.persistence."/persistent/impermanence".users.one6th.directories = [
+    ".cache/JetBrains"
+    ".config/JetBrains"
+    ".gradle"
+    ".java"
+    ".local/share/JetBrains"
+    ".m2"
+  ];
+
+  home-manager.users.one6th = import ./home.nix;
+
+  networking.hostName = "laptop1";
+
+  services = {
+    logind = {
+      powerKey = "ignore";
+      suspendKey = "ignore";
+      suspendKeyLongPress = "ignore";
+    };
+    smartdns.settings.hosts-file = config.sops.secrets.hosts.path;
+  };
+
+  sops = {
+    defaultSopsFile = ./secrets.yaml;
+    secrets = {
+      "company.network" = {
+        owner = "systemd-network";
+        path = "/etc/systemd/network/company.network";
+      };
+      hosts.mode = "0444";
+      password_root.neededForUsers = true;
+      password_one6th.neededForUsers = true;
+      u2f_keys.mode = "0444";
+    };
+  };
+
+  users.users = {
+    one6th.hashedPasswordFile = config.sops.secrets.password_one6th.path;
+    root.hashedPasswordFile = config.sops.secrets.password_root.path;
+  };
+}
---
+++ nixos/laptop1/home.nix
@@ -0,0 +1,39 @@
+{ pkgs, self, ... }:
+
+{
+  imports = builtins.attrValues self.homeModules;
+
+  home = {
+    file = {
+      ".jdks/jdk8".source = "${pkgs.jdk8}/lib/openjdk";
+      ".jdks/jdk11".source = "${pkgs.jdk11}/lib/openjdk";
+      ".jdks/jdk17".source = "${pkgs.jdk17}/lib/openjdk";
+      ".jdks/jdk21".source = "${pkgs.jdk21}/lib/openjdk";
+    };
+    packages = with pkgs; [
+      gradle
+      maven
+
+      jetbrains.idea-community
+    ];
+  };
+
+  wayland.windowManager.hyprland.settings.device = [
+    {
+      accel_profile = "adaptive";
+      middle_button_emulation = true;
+      name = "pnp0c50:00-093a:0255-touchpad";
+      natural_scroll = true;
+    }
+  ];
+
+  xdg.desktopEntries.idea-community = {
+    exec = builtins.toString (
+      pkgs.writeShellScript "idea-community" ''
+        idea-community -Dawt.toolkit.name=WLToolkit
+      ''
+    );
+    icon = "idea-community";
+    name = "IntelliJ IDEA CE";
+  };
+}
