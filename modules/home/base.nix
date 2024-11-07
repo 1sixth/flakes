55a5d791992bccb58bfed04eae44a29471de38f0
1sixth <1sixth@shinta.ro>
2024-11-07 14:09:53 +0800

    Revert "home.base: let yt-dlp prioritize av1"
    This reverts commit c1596d7a67c229ae7fefb931ef4f44e6448c90db.

--- modules/home/base.nix
+++ modules/home/base.nix
@@ -169,7 +169,6 @@
         cookies-from-browser = "firefox";
         embed-chapters = true;
         extractor-args = "youtube:skip=dash,translated_subs";
-        format-sort = "res,vcodec:av01";
         output = "$PWD/%(title)s.%(ext)s";
         remux-video = "mkv";
         sub-langs = "ai-zh,en.*,zh.*";
