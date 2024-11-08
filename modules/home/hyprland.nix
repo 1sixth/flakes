6fd13e3da3b54a9c658dac6d21d19186c4e8412f
1sixth <1sixth@shinta.ro>
2024-11-09 00:08:52 +0800

    home.hyprland: update smart gaps config

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -113,9 +113,20 @@
         "workspace 3 silent, class:mpv"
         "workspace 3 silent, class:org.kde.okular"
         "workspace 4 silent, class:thunderbird"
+
+        # smart gaps, see https://wiki.hyprland.org/Configuring/Workspace-Rules/#smart-gaps
+        "bordersize 0, floating:0, onworkspace:w[t1]"
+        "rounding 0, floating:0, onworkspace:w[t1]"
+        "bordersize 0, floating:0, onworkspace:w[tg1]"
+        "rounding 0, floating:0, onworkspace:w[tg1]"
+        "bordersize 0, floating:0, onworkspace:f[1]"
+        "rounding 0, floating:0, onworkspace:f[1]"
       ];
       workspace = [
-        "w[t1], gapsout:0, border:0"
+        # smart gaps
+        "w[t1], gapsout:0, gapsin:0"
+        "w[tg1], gapsout:0, gapsin:0"
+        "f[1], gapsout:0, gapsin:0"
       ];
       xwayland.force_zero_scaling = true;
     };
