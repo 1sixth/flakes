bf80f6bd0eeaaeb2bb2a3cb7f1d1fab05816b081
1sixth <1sixth@shinta.ro>
2024-11-20 09:33:14 +0800

    Revert "nrt0: redlib: remove override"
    This reverts commit a54911341604218165fc6bdaa2ae0364d355597c.

--- nixos/nrt0/redlib.nix
+++ nixos/nrt0/redlib.nix
@@ -18,7 +18,21 @@
   };
 
   services = {
-    redlib.enable = true;
+    redlib = {
+      enable = true;
+      package = pkgs.redlib.overrideAttrs (old: rec {
+        src = pkgs.fetchFromGitHub {
+          owner = "redlib-org";
+          repo = "redlib";
+          rev = "d3ba5f3efb6825f4b0454523dae472b2adda3066";
+          hash = "sha256-XT6I7EvSPY2KdKTeJyLQ2tu6iiXipy5PB4OgyEsrMhM=";
+        };
+        cargoDeps = old.cargoDeps.overrideAttrs {
+          inherit src;
+          outputHash = "sha256-6a//gKo4YYsXdf5mxktD3gmeoUdKujUPUUO9EOnf8ao=";
+        };
+      });
+    };
     traefik = {
       dynamicConfigOptions.http = {
         routers.redlib = {
